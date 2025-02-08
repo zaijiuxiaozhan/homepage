9a935150a5bc86447fb2c656e1fae837b8dc5d23
zaijiuxiaozhan <qichangliu7@163.com>
2025-02-08 14:33:43 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -32,13 +32,21 @@ const bgUrl = ref(null);
 const imgTimeout = ref(null);
 const emit = defineEmits(["loadComplete"]);
 
-// 固定壁纸链接
-const bgUrl = { value: "/images/background1.jpg" };
+// 壁纸随机数
+// 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
+const bgRandom = 1;
 
 // 更换壁纸链接
 const changeBg = (type) => {
-    // 无论 type 是什么值，都使用固定的壁纸
-    bgUrl.value = "/images/background1.jpg";
+  if (type == 0) {
+    bgUrl.value = `/images/background${bgRandom}.jpg`;
+  } else if (type == 1) {
+    bgUrl.value = "https://api.dujin.org/bing/1920.php";
+  } else if (type == 2) {
+    bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
+  } else if (type == 3) {
+    bgUrl.value = "https://api.vvhan.com/api/wallpaper/acg";
+  }
 };
 
 // 图片加载完成
